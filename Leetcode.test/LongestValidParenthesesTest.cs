b940e78d988d8d82ca5323c7b6a3f37907aed0f8
Macbook Pro <rhedia@gmail.com>
2018-03-31 15:46:11 +0200

    Add new parenthesis test case

--- Leetcode.test/LongestValidParenthesesTest.cs
+++ Leetcode.test/LongestValidParenthesesTest.cs
@@ -19,6 +19,7 @@ namespace Leetcode.test
             Assert.Equal(6, LongestValidParentheses.Execute(")(())))(())())"));
             Assert.Equal(6, LongestValidParentheses.Execute("()(())"));
             Assert.Equal(10, LongestValidParentheses.Execute(")(())(()()))("));
+            Assert.Equal(2, LongestValidParentheses.Execute("()"));
         }
     }
 }
